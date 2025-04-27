25b325b746a669f0bdcfdff2bad8aacc14168d94
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-04-27 17:50:27 +0300

    Update plantsy

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  function handleSearchChange(term) {
+    setSearchTerm(term);
+  }
+
+  function handleToggleSoldOut(id) {
+    const updatedPlants = plants.map((plant) =>
+      plant.id === id ? { ...plant, soldOut: !plant.soldOut } : plant
+    );
+    setPlants(updatedPlants);
+  }
+
+  const plantsToDisplay = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={handleSearchChange} />
+      <PlantList plants={plantsToDisplay} onToggleSoldOut={handleToggleSoldOut} />
     </main>
   );
 }
